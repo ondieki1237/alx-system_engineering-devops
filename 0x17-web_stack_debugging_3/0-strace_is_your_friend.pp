32b2e5fee9afe64fefbe2d070798bad9f13aa178
ondieki1237 <makoriseth1237@gmail.com>
2024-08-19 04:36:36 +0300

    makori

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,6 @@
-# 0-strace_is_your_friend.pp
-
-exec { 'fix-apache':
-  command => '/usr/sbin/service apache2 restart',
-  onlyif  => 'test -f /etc/apache2/sites-enabled/000-default.conf',
-}
-
-file { '/etc/apache2/sites-enabled/000-default.conf':
-  ensure  => 'present',
-  source  => '/etc/apache2/sites-available/000-default.conf',
-  notify  => Exec['fix-apache'],
-}
-
-exec { 'fix-wordpress':
-  command => '/usr/sbin/service apache2 restart',
-  onlyif  => 'grep -q "Error log" /var/log/apache2/error.log',
-}
-
+# Creat a manifest that fix all termintion of phpp.
+#
+# exec { 'fix_phpp':
+#   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+#     path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+#     }
