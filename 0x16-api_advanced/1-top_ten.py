9b9b5c7f3cd301bd76e61ca37a7a535fe256dd50
ondieki1237 <makoriseth1237@gmail.com>
2024-08-20 02:10:28 +0000

    maks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
 
 
 def top_ten(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    try:
-        count = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
-            subreddit), headers=head).json().get('data').get('children')
-        print('\n'.join([dic.get('data').get('title')
-                         for dic in count][:10]))
-    except:
-        print('None')
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
