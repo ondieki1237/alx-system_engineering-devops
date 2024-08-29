dbdda492d4ad3fe80646a5c99c3f6b7b38727e30
ondieki1237 <makoriseth1237@gmail.com>
2024-08-29 05:58:36 +0300

    maks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,23 +6,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to return the number of subscribers of a subreddit
-
-    Keyword arguments: subreddit (a string)
-    Return: return the number of subscribers of a subreddit
-    """
-    if not subreddit or not isinstance(subreddit, str):
-        print("None")
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditBot/1.0"}
-
+    headers = {'User-Agent': 'my_custom_agent'}
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
+            return data['data'].get('subscribers', 0)
         else:
             return 0
-    except Exception as e:
-        return 0       
+    except Exception:
+        return 0
